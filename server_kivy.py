3b7e0181990241d9429b846ca3f274a61c0c2da9
= <=>
2024-05-13 16:16:37 -0400

    server_kivy and .kv layout working for test buttons

--- server_kivy.py
+++ server_kivy.py
@@ -1,29 +1,23 @@
+# THIS IS THE SERVER LOGIC FILE
+
 import kivy
 from kivy.app import App
 from kivy.clock import Clock
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.uix.video import Video
 from kivy.core.window import Window
 
+class ServerLayout(BoxLayout):
+    def test(self, arg1):
+        print(f"Test Pressed {arg1}")
+    pass
+    
 
 class ServerApp(App):
     def build(self):
-        # Set the window properties
         Window.size = (Window.width, Window.height)
-        Window.borderless = True
-     #    Window.fullscreen = True
-
-        # Define the fade function
-        self.fadeAmount = 0.01
-        def fade_image(dt):
-            image = self.root.ids.video
-            if image.opacity < 0 or image.opacity > 1:
-                self.fadeAmount *= -1     
-            image.opacity += self.fadeAmount
-
-        # Start the clock to call the fade function every 2 seconds
-        Clock.schedule_interval(fade_image, 0.01)
+        return ServerLayout()
 
 
 if __name__ == "__main__":
