8724a0ba5093867bb55fef809272d86a7b880dcd
= <=>
2024-06-29 17:51:50 -0400

    renamed and cleaned

--- client.py
+++ client.py
@@ -1,22 +1,23 @@
+# THE CLIENT LOGIC FILE
+import kivy
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.clock import mainthread
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.image import Image
+from kivy.uix.video import Video
+from kivy.core.window import Window
+
+
+# networking includes
 import socket
 import struct
 import marshal
 import threading
 
+# network settings
 MCAST_GRP = '224.1.1.1'
 MCAST_PORT = 8080
-
-class MulticastListenerThread(threading.Thread):
-    def __init__(self, sock):
-        threading.Thread.__init__(self)
-        self.sock = sock
-
-    def run(self):
-        while True:
-            received_data = self.sock.recv(10240)
-            received_dict = marshal.loads(received_data)
-            received_dict['sender_ip'] = socket.inet_ntoa(received_data[0:4])  # Extract sender's IP address
-            print(f"received: {received_dict}")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -25,5 +26,103 @@ mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
 
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-listener_thread = MulticastListenerThread(sock)
-listener_thread.start()
+
+class MulticastListenerThread(threading.Thread):
+    def __init__(self, sock, root):
+        threading.Thread.__init__(self)
+        self.sock = sock
+        self.root = root
+
+    def run(self):
+        while True:
+            received_data = self.sock.recv(10240)
+            received_dict = marshal.loads(received_data)
+            received_dict['sender_ip'] = socket.inet_ntoa(received_data[0:4])  # Extract sender's IP address
+            print(f"received: {received_dict}")
+            if 'type' in received_dict:
+                if received_dict['type'] == 'test':
+                    self.root.updateStatus(received_dict['value'])
+                    
+                elif received_dict['type'] == 'image':
+                    self.root.updateStatus(received_dict['value'])
+                    self.root.playImage(received_dict['value'])
+                    
+                elif received_dict['type'] == 'video':
+                    self.root.updateStatus(received_dict['value'])
+                    self.root.playVideo(received_dict['value'])
+
+
+
+
+class ClientLayout(FloatLayout):
+    
+    def on_duration_change(instance, value=None):
+        print('The duration of the video is', value)
+    def on_position_change(instance, value=None):
+        # print('The position in the video is', value)
+        pass
+    def on_eos_change(instance, value=None):
+        print('The EOS of the video is', value)
+    
+    @mainthread
+    def toTop(self, player):
+        self.ids.image1.opacity = 0
+        self.ids.video1.opacity = 0
+        player.opacity = 1   #on top
+        
+    def updateStatus(self, text):
+        self.ids.status_text.text = text
+    @mainthread
+    def playImage(self, path, options=None):
+        img = self.ids.image1
+        img.source = path
+        self.toTop(img)
+        self.updateStatus(f"playing image: {path}")
+        pass
+    @mainthread
+    def playVideo(self, path, options=None): 
+        vid = self.ids.video1
+        vid.source = path
+        self.toTop(vid)
+        
+        vid.state = 'play'
+        self.updateStatus(f"playing video: {path}")
+        pass
+    
+    # # # Define the fade function
+    # # self.fadeAmount = 0.01
+    # # def fade_image(dt):
+    # #     image = self.root.ids.video
+    # #     if image.opacity < 0 or image.opacity > 1:
+    # #         self.fadeAmount *= -1     
+    # #     image.opacity += self.fadeAmount
+
+    # # # Start the clock to call the fade function every 2 seconds
+    # # Clock.schedule_interval(fade_image, 0.01)
+    
+    
+        
+    
+class ClientApp(App):
+        
+    def build(self):
+        # Set the window properties
+        Window.size = (Window.width, Window.height)
+        # Window.borderless = True
+        # Window.fullscreen = True
+        Window.show_cursor = False
+        
+        root = ClientLayout()
+        # root.playImage("media/test1.jpg")
+        # root.playVideo("media/test3.mp4")
+        
+        
+        listener_thread = MulticastListenerThread(sock, root)
+        listener_thread.setDaemon(True)
+        listener_thread.start() 
+        
+        return root
+
+
+if __name__ == "__main__":
+    ClientApp().run()
--- server.py
+++ server.py
@@ -1,6 +1,15 @@
+# THIS IS THE SERVER LOGIC FILE
+
+import kivy
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+
+# NETWORKING
 import socket
 from time import sleep, time
 import marshal
+import threading
 
 MCAST_GRP = '224.1.1.1'
 MCAST_PORT = 8080
@@ -15,9 +24,46 @@ def broadcast(data_dict):
         sock.sendto(marshal.dumps(data_dict), (MCAST_GRP, MCAST_PORT))
     except Exception as e:
         print(f"Error sending data: {e}")
+        
 
-while True:
-    print("sending multicast packet...")
-    message = {"hello": "world"}
-    broadcast(message) 
-    sleep(2)
+class MulticastBroadcasterThread(threading.Thread):
+    def __init__(self, sock):
+        threading.Thread.__init__(self)
+        self.sock = sock
+
+    def run(self):
+        while True:
+            print("sending multicast packet...")
+            message = {"hello": "world"}
+            broadcast(message) 
+            sleep(2)
+            
+            
+
+class ServerLayout(BoxLayout):
+    def test(self, arg1):
+        print(f"Test Pressed {arg1}")
+        broadcast({"type":"test", "value":f"button {arg1}"}) 
+    
+    def triggerImage(self, path):
+        print(f"trigger image {path}")
+        broadcast({"type":"image", "value":path}) 
+
+    def triggerVideo(self, path):
+        print(f"trigger video {path}")
+        broadcast({"type":"video", "value":path}) 
+    
+
+class ServerApp(App):
+    def build(self):
+        Window.size = (Window.width, Window.height)
+        
+        broadcaster_thread = MulticastBroadcasterThread(sock)
+        broadcaster_thread.setDaemon(True)
+        broadcaster_thread.start() 
+        
+        return ServerLayout()
+
+
+if __name__ == "__main__":
+    ServerApp().run()
